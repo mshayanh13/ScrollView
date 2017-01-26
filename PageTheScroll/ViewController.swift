a08854e522e00a59bb90e391e1b5bc24c55545f4
mshayanh13 <mohammadhemani@gmail.com>
2017-01-26 16:40:23 -0600

    Added scrolling on edges

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
+    
     var images = [UIImageView]()
+    let MAX_PAGE = 2
+    let MIN_PAGE = 0
+    var currentPage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +31,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         var contentWidth: CGFloat = 0.0
         
-        print("Scrollview width: \(scrollView.frame.width)")
+        //print("Scrollview width: \(scrollView.frame.width)")
         
         let scrollWidth = scrollView.frame.size.width
     
@@ -40,17 +44,43 @@ class ViewController: UIViewController {
             
             newX = scrollWidth / 2 + scrollWidth * CGFloat(x) //newX = view.frame.midX + view.frame.size.width * CGFloat(x)
             
-            contentWidth += scrollWidth * CGFloat(x)//newX
+            contentWidth += newX //scrollWidth * CGFloat(x)
             
             scrollView.addSubview(imageView)
             
             imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
+        self.images[self.currentPage].transform = CGAffineTransform.init(scaleX: 1.14, y: 1.4)
         
         scrollView.clipsToBounds = false
         
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
+    @IBAction func detectSwipe(_ sender: UISwipeGestureRecognizer) {
+        if currentPage < MAX_PAGE && sender.direction == UISwipeGestureRecognizerDirection.left {
+            moveScrollView(direction: 1)
+        }
+        
+        if currentPage > MIN_PAGE && sender.direction == UISwipeGestureRecognizerDirection.right {
+            moveScrollView(direction: -1)
+        }
+    }
+    
+    func moveScrollView(direction: Int) {
+        currentPage = currentPage + direction
+        let point: CGPoint = CGPoint(x: scrollView.frame.size.width * CGFloat(currentPage), y: 0.0)
+        scrollView.setContentOffset(point, animated: true)
+        
+        UIView.animate(withDuration: 0.4) {
+            self.images[self.currentPage].transform = CGAffineTransform.init(scaleX: 1.4, y: 1.4)
+            
+            for x in 0..<self.images.count {
+                if x != self.currentPage {
+                    self.images[x].transform = CGAffineTransform.identity
+                }
+            }
+        }
+    }
 }
 
